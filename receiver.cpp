78c84b484e150f278edcac6f605a27ea2e5007f9
Christopher Johnson <chrisajohnson1988@gmail.com>
2016-03-12 16:19:24 +0000

    Reading only from arduino node

--- receiver.cpp
+++ receiver.cpp
@@ -1,9 +1,8 @@
 #include <RF24/RF24.h>
 #include <RF24Network/RF24Network.h>
+#include <cstdlib>
 #include <iostream>
-#include <ctime>
 #include <stdio.h>
-#include <time.h>
 #include <curl/curl.h>
 #include <sstream>
 
@@ -11,6 +10,7 @@ RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
 RF24Network network(radio);
 
 const uint16_t MY_ADDR = 00;
+const uint16_t ARDUINO_ADDR = 01;
 
 struct Payload {
   float in_temp;
@@ -26,44 +26,49 @@ struct Payload {
  * from the arduino sensor node then submit its values to thingspeak
  */
 int main(int argc, char *argv[]) {
-  char* api_key = std::getenv("THINGSPEAK_API_KEY");
-  radio.begin();
-  radio.enableDynamicPayloads();
-  radio.setAutoAck(1);
-  radio.setRetries(15,15);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setCRCLength(RF24_CRC_8);
-  delay(5);
-  network.begin(108, MY_ADDR);
-  radio.printDetails();
+  if(char* api_key = std::getenv("THINGSPEAK_API_KEY")) {
+    radio.begin();
+    radio.enableDynamicPayloads();
+    radio.setAutoAck(1);
+    radio.setRetries(15,15);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setCRCLength(RF24_CRC_8);
+    network.begin(108, MY_ADDR);
+    radio.printDetails();
 
-  while(1) {
-    network.update();
-    while ( network.available() ) {
-      RF24NetworkHeader header;
-      Payload payload;
-      network.read(header,&payload,sizeof(payload));
+    while(1) {
+      network.update();
+      while ( network.available() ) {
+        RF24NetworkHeader header;
+        Payload payload;
+        network.read(header,&payload,sizeof(payload));
 
-      std::stringstream data;
-      data << "https://api.thingspeak.com/update?"
-           << "api_key=" << api_key          << "&"
-           << "field1="  << payload.in_temp  << "&"
-           << "field2="  << payload.in_hum   << "&"
-           << "field3="  << payload.out_temp << "&"
-           << "field4="  << payload.out_hum  << "&"
-           << "field5="  << payload.soil1    << "&"
-           << "field6="  << payload.voltage;
-      
-       CURL *curl;
+        if(header.from_node == ARDUINO_ADDR) {
+          std::stringstream data;
+          data << "https://api.thingspeak.com/update?"
+               << "api_key=" << api_key          << "&"
+               << "field1="  << payload.in_temp  << "&"
+               << "field2="  << payload.in_hum   << "&"
+               << "field3="  << payload.out_temp << "&"
+               << "field4="  << payload.out_hum  << "&"
+               << "field5="  << payload.soil1    << "&"
+               << "field6="  << payload.voltage;
 
-       curl_global_init(CURL_GLOBAL_ALL);
-       curl = curl_easy_init();
-       curl_easy_setopt(curl, CURLOPT_URL, data.str().c_str());
-       curl_easy_perform(curl);
-     }
+           CURL *curl;
+
+           curl_global_init(CURL_GLOBAL_ALL);
+           curl = curl_easy_init();
+           curl_easy_setopt(curl, CURLOPT_URL, data.str().c_str());
+           curl_easy_perform(curl);
+           std::cout << " Packet Send\n";
+         }
+      }
+    }
+    curl_global_cleanup();
+    return EXIT_SUCCESS;
   }
-  curl_global_cleanup();
-  return 0;
+  std::cout << "Expected to THINGSPEAK_API_KEY environment variable to be defined\n";
+  return EXIT_FAILURE;
 }
 
