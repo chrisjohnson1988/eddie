a714e5b3112fa3a3e1eed3b9dc4562ae0be0810c
Christopher Johnson <chrisajohnson1988@gmail.com>
2016-07-14 21:13:22 +0100

    Adding a watchdog which will reset the radio if no packets received for a while

--- receiver.cpp
+++ receiver.cpp
@@ -10,6 +10,7 @@
 RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);  
 RF24Network network(radio);
 
+const int RESET_RADIO_AFTER_NO_PACKET = 30;
 const uint16_t MY_ADDR = 00;
 const uint16_t EDDIE_ADDR = 01;
 const uint16_t MINI_EDDIE_ADDR = 011;
@@ -29,6 +30,17 @@ struct PayloadRemoteSoil {
   uint16_t soil1;
 };
 
+void initRadio() {
+  radio.begin();
+  radio.enableDynamicPayloads();
+  radio.setAutoAck(1);
+  radio.setRetries(15,15);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setCRCLength(RF24_CRC_8);
+  network.begin(108, MY_ADDR);
+}
+
 /**
  * Start looping listening to packets from the NRF24 radio. If one is received 
  * from the arduino sensor node then submit its values to thingspeak
@@ -37,18 +49,12 @@ int main(int argc, char *argv[]) {
   char* eddie_key      = std::getenv("EDDIE_API_KEY");
   char* mini_eddie_key = std::getenv("MINI_EDDIE_API_KEY");
   if(eddie_key && mini_eddie_key) {
-    radio.begin();
-    radio.enableDynamicPayloads();
-    radio.setAutoAck(1);
-    radio.setRetries(15,15);
-    radio.setDataRate(RF24_250KBPS);
-    radio.setPALevel(RF24_PA_MAX);
-    radio.setCRCLength(RF24_CRC_8);
-    network.begin(108, MY_ADDR);
+    initRadio();
     radio.printDetails();
     curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl;
     curl = curl_easy_init();
+    int lastreceived = 0;
 
     while(1) {
       network.update();
@@ -56,6 +62,7 @@ int main(int argc, char *argv[]) {
       while ( network.available() ) {
         RF24NetworkHeader header;
         network.peek(header);
+        lastreceived = 0; //reset radio no packet count
 
         if(header.from_node == EDDIE_ADDR) {
           Payload payload;
@@ -88,6 +95,11 @@ int main(int argc, char *argv[]) {
           curl_easy_perform(curl);
           std::cout << " Mini Eddie Packet Sent\n";
         }
+      }
+      if(lastreceived++ > RESET_RADIO_AFTER_NO_PACKET) {
+        std::cout << "No packets for a while. Resetting radio\n";
+        initRadio();
+        lastreceived = 0;
       }
       delay(2000);
     }
